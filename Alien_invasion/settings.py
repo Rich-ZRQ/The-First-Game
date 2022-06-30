d578bb11ee195c65fa5cb479c5efcdc30fbbe1aa
Rich <2656496522@qq.com>
2022-06-30 21:35:04 +0800

    The third commit

--- Alien_invasion/settings.py
+++ Alien_invasion/settings.py
@@ -2,17 +2,23 @@ class Settings:
     '''存储游戏《外星人入侵》中所有设置的类'''
     def __init__(self):
         '''初始化设置'''
-        #屏幕设置
+        # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
-        #飞船速度
+        # 飞船设置
         self.ship_speed = 1.5
-        #子弹设置
-        self.bullet_speed = 1.0
+        self.ship_limit = 3
+        # 子弹设置
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        # 外星人设置
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移，为-1表示向左移。
+        self.fleet_direction = 1
 
 
--- Alien_invasion/ship.py
+++ Alien_invasion/ship.py
@@ -9,14 +9,14 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
-        #加载飞船图像并获取其外接矩形。
+        # 加载飞船图像并获取其外接矩形。
         self.image = pygame.image.load('D:/Python_game/Ship_images/ship.bmp')
         self.rect = self.image.get_rect()
-        #对于每艘新飞船，都将其放在屏幕底部的中央。
+        # 对于每艘新飞船，都将其放在屏幕底部的中央。
         self.rect.midbottom = self.screen_rect.midbottom
-        #在飞船的属性x中存储小数值。
+        # 在飞船的属性x中存储小数值。
         self.x = float(self.rect.x)
-        #移动标志
+        # 移动标志
         self.moving_right = False
         self.moving_left = False
 
@@ -26,12 +26,17 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-        #根据self.x更新rect对象。
+        # 根据self.x更新rect对象。
         self.rect.x = self.x
 
     def blitme(self):
         '''在指定为之绘制飞船'''
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        '''让飞船在屏幕低端居中'''
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
 
 
